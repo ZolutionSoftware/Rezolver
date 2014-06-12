37ad28bf26aa7aaa9160cef4028889d7e06548f5
Andras Zoltan <andras@zoltanclan.co.uk>
2014-06-12 13:02:17 +0100

    vs missed another file!

---
+++ Rezolver/Rezolver/RezolverScopePath.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rezolver
+{
+	/// <summary>
+	/// Represents a path to a named rezolver scope.  Optimised for walking the path in both directions after creation.
+	/// 
+	/// Please note - this class is not thread-safe.
+	/// </summary>
+	public class RezolverScopePath
+	{
+		public const string DefaultPathSeparator = ".";
+		private static readonly Regex _rxNoWhitespace = new Regex(@"\s");
+
+		public string Path { get; private set; }
+		public string PathSeparator { get; private set; }
+
+		private string[] Items { get; set; }
+
+		public string CurrentItem
+		{
+			get
+			{
+				if(_currentItem <0 || _currentItem >= Items.Length)
+					throw new InvalidOperationException(/*TODO: add resourced exception*/);
+				return Items[_currentItem];
+			}
+		}
+
+		/// <summary>
+		/// used when walking the path
+		/// </summary>
+		private int _currentItem = -1;
+
+		public RezolverScopePath(string path, string pathSeparator = null)
+		{
+			PathSeparator = pathSeparator ?? DefaultPathSeparator; 
+			path.MustNotBeNull("path");
+			Path = path;
+
+			var items = path.Split(PathSeparator.ToCharArray());
+			foreach (var item in items)
+			{
+				if (string.IsNullOrWhiteSpace(item))
+					throw new ArgumentException(Resources.Exceptions.PathIsInvalid, "path");
+				if(item.Length == 0)
+					throw new ArgumentException(Resources.Exceptions.PathIsInvalid, "path");
+				if(_rxNoWhitespace.IsMatch(item))
+					throw new ArgumentException(Resources.Exceptions.PathIsInvalid, "path");
+			}
+
+			Items = items;
+		}
+
+		public RezolverScopePath(RezolverScopePath source)
+		{
+			source.MustNotBeNull("source");
+			Path = source.Path;
+			Items = source.Items;
+			PathSeparator = source.PathSeparator;
+		}
+
+		/// <summary>
+		/// Creates a ResolverScopePath from the given string, using the default
+		/// path separator.
+		/// </summary>
+		/// <param name="source"></param>
+		/// <returns></returns>
+		public static implicit operator RezolverScopePath(string source)
+		{
+			return new RezolverScopePath(source);
+		}
+
+		//public IEnumerator<string> GetEnumerator()
+		//{
+		//	return Items.AsEnumerable().GetEnumerator();
+		//}
+
+		//IEnumerator IEnumerable.GetEnumerator()
+		//{
+		//	return GetEnumerator();
+		//}
+		public bool MoveNext()
+		{
+			return ++_currentItem < Items.Length;
+		}
+	}
+}
