2e90701de2725d24471606f159c476a5e75d8ba0
LordZoltan <andras@zoltanclan.co.uk>
2014-08-09 22:27:57 +0000

    moving from vm to real machine

--- Rezolver/Rezolver.Tests/LifeTimeRezolverContainerTests.cs
+++ Rezolver/Rezolver.Tests/LifeTimeRezolverContainerTests.cs
@@ -15,6 +15,7 @@ namespace Rezolver.Tests
 
 		public class DisposableType : ITestDisposable
 		{
+			public static int TotalDisposeCount = 0;
 			int _disposeCount = 0;
 
 			public int DisposeCount
@@ -27,6 +28,7 @@ namespace Rezolver.Tests
 
 			public void Dispose()
 			{
+				++TotalDisposeCount;
 				++_disposeCount;
 				if (!_disposed)
 				{
@@ -35,12 +37,18 @@ namespace Rezolver.Tests
 			}
 		}
 
-		[TestMethod]
-		public void ShouldDisposeOnceOly()
+		private static Mock<IRezolverContainer> CreateMock()
 		{
 			Mock<IRezolverContainer> parentContainerMock = new Mock<IRezolverContainer>();
 			parentContainerMock.Setup(c => c.CreateLifetimeContainer()).Returns(() => new LifetimeRezolverContainer(parentContainerMock.Object));
-			parentContainerMock.Setup(c => c.Resolve(typeof (ITestDisposable), null, null)).Returns(() => new DisposableType());
+			parentContainerMock.Setup(c => c.Resolve(typeof(ITestDisposable), null, null)).Returns(() => new DisposableType());
+			return parentContainerMock;
+		}
+
+		[TestMethod]
+		public void ShouldDisposeOnceOly()
+		{
+			Mock<IRezolverContainer> parentContainerMock = CreateMock();
 			ITestDisposable instance = null;
 			using (var lifetime = parentContainerMock.Object.CreateLifetimeContainer())
 			{
@@ -49,9 +57,42 @@ namespace Rezolver.Tests
 
 			Assert.IsTrue(instance.Disposed);
 			Assert.AreEqual(1, instance.DisposeCount);
+		}
 
+		[TestMethod]
+		public void ShouldDisposeTwoInstances()
+		{
+			var mockContainer = CreateMock();
+			int totalInstanceCount = DisposableType.TotalDisposeCount;
+			ITestDisposable instance = null;
+			ITestDisposable instance2 = null;
+			using(var lifetime = mockContainer.Object.CreateLifetimeContainer())
+			{
+				instance = (ITestDisposable)lifetime.Resolve(typeof(ITestDisposable));
+				instance2 = (ITestDisposable)lifetime.Resolve(typeof(ITestDisposable));
+				Assert.AreNotSame(instance, instance2);
+			}
 
+			Assert.IsTrue(instance.Disposed);
+			Assert.IsTrue(instance2.Disposed);
+			Assert.AreEqual(1, instance.DisposeCount);
+			Assert.AreEqual(1, instance2.DisposeCount);
+			Assert.AreEqual(totalInstanceCount + 2, DisposableType.TotalDisposeCount);
+		}
 
+		[TestMethod]
+		public void ShouldDisposeNestedScope()
+		{
+			var mockContainer = CreateMock();
+			ITestDisposable instance = null;
+			using(var lifetime = mockContainer.Object.CreateLifetimeContainer())
+			{
+				var lifetime2 = lifetime.CreateLifetimeContainer();
+				instance = (ITestDisposable)lifetime2.Resolve(typeof(ITestDisposable));
+			}
+
+			Assert.IsTrue(instance.Disposed);
+			Assert.AreEqual(1, instance.DisposeCount);
 		}
 	}
 }
