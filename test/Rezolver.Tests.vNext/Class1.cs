27615dec8ccfec6193e01a651a4aca64ae10f2e3
LordZoltan <andras@zoltanclan.co.uk>
2015-04-28 23:46:03 +0100

    xunit test project building and detecting tests in VS.  ready to roll :)

---
+++ test/Rezolver.Tests.vNext/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace Rezolver.Tests.vNext
+{
+    public class Class1
+    {
+		[Fact]
+        public void HelloXUnit()
+        {
+			Assert.True(true);
+        }
+    }
+}
