79a43044d1ce6cdedec19903c485e29b60852361
LordZoltan <andras@zoltanclan.co.uk>
2016-06-22 16:31:55 +0100

    a bit more customisation done on the docfx template now - including a few hacks to pick up a custom build of bootstrap

---
+++ doc/Rezolver.Documentation/Gruntfile.js
@@ -0,0 +1,39 @@
+﻿/// <binding BeforeBuild='default' />
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        less: {
+            dev: {
+                options: {
+                    sourceMap: true,
+                    dumpLineNumbers: 'comments',
+                    relativeUrls: true,
+                    plugins: [
+                        new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions"]})
+                    ]
+                },
+                files: {
+                    'styles/bootstrap-rezolver.debug.css': 'styles/bootstrap-rezolver.less'
+                }
+            },
+            prod: {
+                options: {
+                    compress: true,
+                    relativeUrls: true,
+                    plugins: [
+                            new (require('less-plugin-autoprefix'))({ browsers: ["last 2 versions"] })
+                    ]
+                },
+                files: {
+                    'styles/bootstrap-rezolver.css': 'styles/bootstrap-rezolver.less'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.registerTask('default', ['less']);
+    grunt.registerTask('prod', ['less:prod']);
+    grunt.registerTask('dev', ['less:dev']);
+};
