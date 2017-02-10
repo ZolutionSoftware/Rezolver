cdf0e3ca4408f6217259e58ba44d4100bb7ad1a2
Andras Zoltan <andras@zoltanclan.co.uk>
2017-02-10 00:15:40 +0000

    It looks like it's working - a couple of deep tests purely of the simpler target.  Also need to think about how to create scopes seamlessly from containers and other scopes so I can get that finalised.
    Few more tests yet though

--- src/Rezolver.Compilation.Expressions/ExpressionBuilderBase.cs
+++ src/Rezolver.Compilation.Expressions/ExpressionBuilderBase.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Rezolver.Compilation.Expressions
@@ -22,6 +23,24 @@ namespace Rezolver.Compilation.Expressions
 	/// method</remarks>
 	public abstract class ExpressionBuilderBase : IExpressionBuilder
 	{
+		/// <summary>
+		/// Gets a MethodInfo object for the <see cref="IContainerScope.Resolve(ResolveContext, Func{ResolveContext, object}, ScopeActivationBehaviour)"/>
+		/// method for help in generating scope-interfacing code.
+		/// </summary>
+		protected MethodInfo IContainerScope_Resolve_Method { get; } = MethodCallExtractor.ExtractCalledMethod(
+				(IContainerScope s) => s.Resolve(
+					(ResolveContext)null,
+					(Func<ResolveContext, object>)null,
+					ScopeActivationBehaviour.None));
+
+		/// <summary>
+		/// Gets a MethodInfo object for the <see cref="ITarget.SelectScope(ResolveContext)"/> method for
+		/// help in generating scope-interfacing code.
+		/// </summary>
+		protected MethodInfo ITarget_SelectScope_Method { get; } = MethodCallExtractor.ExtractCalledMethod(
+				(ITarget t) => t.SelectScope(null)
+				);
+
 		/// <summary>
 		/// Gets the <see cref="IExpressionCompiler"/> to be used to build the expression for the given target for
 		/// the given context, if different from one passed to this class' implementation of 
@@ -82,56 +101,8 @@ namespace Rezolver.Compilation.Expressions
 				if (convertType != result.Type)
 					result = Expression.Convert(result, convertType);
 
-				if ((context.ScopeBehaviourOverride ?? target.ScopeBehaviour) != ScopeActivationBehaviour.None)
-				{
-					//so the expression needs to be changed so that it becomes something like this:
-					//if(context.Scope != null) return context.Scope.Resolve(context, lambda<targetExpression>);
-					//else return targetExpression
-
-					//TODO: if behaviour == explicit, then a scope is *required* and the expression should throw
-					//an exception if it is null :)
-
-					//TODO: optimise away these methodinfos etc.
-
-					//this will automatically be of type object and will be optimised.
-					var lambda = compiler.BuildResolveLambda(result, context);
-					var scopeResolveMethod = MethodCallExtractor.ExtractCalledMethod(
-						(IContainerScope s) => s.Resolve(
-							(ResolveContext)null, 
-							(Func<ResolveContext, object>)null, 
-							ScopeActivationBehaviour.None));
-					var selectScopeMethod = MethodCallExtractor.ExtractCalledMethod(
-						(ITarget t) => t.SelectScope(null)
-						);
-					//use a shared expression for the scope check so we can optimise away all the nested scope calls
-					//we're likely to be generating.
-					var compareExpr = context.GetOrAddSharedExpression(typeof(bool), "isScoped", () =>
-					{
-						return Expression.Equal(context.ContextScopePropertyExpression, Expression.Default(typeof(IContainerScope)));
-					}, typeof(ExpressionBuilderBase));
-
-					result = Expression.Condition(
-						compareExpr,
-						result, //if null scope, just use the built expression as-is
-						Expression.Convert(	//otherwise - generate a call into the scope's special Resolve method
-							Expression.Call(
-								//the target is called to select the scope it wants
-								Expression.Call(
-									Expression.Constant(target), 
-									selectScopeMethod,
-									context.ResolveContextExpression
-								),
-								scopeResolveMethod,
-								context.ResolveContextExpression,
-								lambda,
-								//feed the target's scope behaviour
-								//(note: I'm thinking this should be derived from the context
-								//unless it's null)
-								Expression.Constant(target.ScopeBehaviour)
-							),
-							convertType
-						));
-				}
+				var scopeBehaviour = context.ScopeBehaviourOverride ?? target.ScopeBehaviour;
+				result = ApplyScoping(scopeBehaviour, result, target, context, compiler);
 
 				return result;
 			}
@@ -139,6 +110,72 @@ namespace Rezolver.Compilation.Expressions
 			{
 				context.PopCompileStack();
 			}
+		}
+
+		/// <summary>
+		/// Called by the <see cref="BuildCore(ITarget, IExpressionCompileContext, IExpressionCompiler)"/> method.
+		/// 
+		/// Applies the scoping behaviour to the <paramref name="builtExpression"/> such that when it is executed it 
+		/// correctly interfaces with the active scope (from the <see cref="ResolveContext"/>) if one is present for the
+		/// given <paramref name="scopeBehaviour"/>.
+		/// </summary>
+		/// <param name="scopeBehaviour">The scoping behaviour for which the expression is to be adapted.
+		/// 
+		/// If this is <see cref="ScopeActivationBehaviour.None"/>, then the base class implementation will not generate
+		/// any scoping code.</param>
+		/// <param name="builtExpression">The expression that was built for the <paramref name="target"/>.</param>
+		/// <param name="target">The target that is currently being compiled and from which the <paramref name="builtExpression" />
+		/// was built.</param>
+		/// <param name="context">The current active compilation context.</param>
+		/// <param name="compiler">The compiler.</param>
+		protected virtual Expression ApplyScoping(ScopeActivationBehaviour scopeBehaviour, Expression builtExpression, ITarget target, IExpressionCompileContext context, IExpressionCompiler compiler)
+		{
+			if (scopeBehaviour == ScopeActivationBehaviour.None)
+				return builtExpression;
+
+			var originalType = builtExpression.Type;
+			//so the expression needs to be rewritten so that it becomes something like this:
+			//if(context.Scope != null) return context.Scope.Resolve(context, lambda<targetExpression>);
+			//else return targetExpression
+
+			//TODO: if behaviour == explicit, then a scope is *required* and the expression should throw
+			//an exception if it is null :)
+
+			//TODO: optimise away these methodinfos etc.
+
+			//this will automatically be of type object and will be optimised.
+			var lambda = compiler.BuildResolveLambda(builtExpression, context);
+			//use a shared expression for the scope check so we can optimise away all the nested scope calls
+			//we're likely to be generating.
+			var compareExpr = context.GetOrAddSharedExpression(typeof(bool), "isScoped", () =>
+			{
+				return Expression.Equal(context.ContextScopePropertyExpression, Expression.Default(typeof(IContainerScope)));
+			}, typeof(ExpressionBuilderBase));
+
+			builtExpression = Expression.Condition(
+				compareExpr,
+				builtExpression, //if null scope, just use the built expression as-is
+				Expression.Convert( //otherwise - generate a call into the scope's special Resolve method
+					Expression.Call(
+						//the target is called to select the scope it wants
+						Expression.Call(
+							Expression.Constant(target),
+							ITarget_SelectScope_Method,
+							context.ResolveContextExpression
+						),
+						IContainerScope_Resolve_Method,
+						context.ResolveContextExpression,
+						lambda,
+						//feed the target's scope behaviour
+						//(note: I'm thinking this should be derived from the context
+						//unless it's null)
+						Expression.Constant(scopeBehaviour)
+					),
+					originalType
+				));
+
+
+			return builtExpression;
 		}
 
 		/// <summary>
