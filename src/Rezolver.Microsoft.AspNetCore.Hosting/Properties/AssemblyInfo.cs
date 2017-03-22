bc022392295762c85e7420212ff784f0d4898e17
Andras Zoltan <andras@zoltanclan.co.uk>
2017-03-22 15:11:31 +0000

    vs2017 migration (#19) unholy mess.   Getting there though :)

--- src/Rezolver.Microsoft.AspNetCore.Hosting/Properties/AssemblyInfo.cs
+++ src/Rezolver.Microsoft.AspNetCore.Hosting/Properties/AssemblyInfo.cs
@@ -9,10 +9,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Rezolver.Microsoft.AspNetCore.Hosting")]
-[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("")]
+//[assembly: AssemblyProduct("Rezolver.Microsoft.AspNetCore.Hosting")]
+//[assembly: AssemblyTrademark("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
