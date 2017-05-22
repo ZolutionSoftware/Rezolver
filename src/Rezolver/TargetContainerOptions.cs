369bc7927d4b1e197f214138d8bc3193ae214db7
Andras Zoltan <andras@zoltanclan.co.uk>
2017-05-22 23:50:00 +0100

    looking much better than my attempt at a new set of types to manage configuration.  This uses the TargetContainer as a 'live' container for configuration.

---
+++ src/Rezolver/TargetContainerOptions.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rezolver
+{
+    public static class testextensions
+    {
+        internal class OptionContainer<TOption> : IDirectTarget, ITarget
+        {
+            public TOption Option { get; }
+
+            bool ITarget.UseFallback => false;
+
+            Type ITarget.DeclaredType => typeof(OptionContainer<TOption>);
+
+            ScopeBehaviour ITarget.ScopeBehaviour => ScopeBehaviour.None;
+
+            ScopePreference ITarget.ScopePreference => ScopePreference.Current;
+
+            public OptionContainer(TOption option)
+            {
+                Option = option;
+            }
+
+            object IDirectTarget.GetValue() => Option;
+
+            bool ITarget.SupportsType(Type type) => typeof(OptionContainer<TOption>) == type;
+        }
+
+        internal class OptionContainer<TService, TOption> : IDirectTarget
+        {
+            public TOption Option { get; }
+
+            bool ITarget.UseFallback => false;
+
+            Type ITarget.DeclaredType => typeof(OptionContainer<TService, TOption>);
+
+            ScopeBehaviour ITarget.ScopeBehaviour => ScopeBehaviour.None;
+
+            ScopePreference ITarget.ScopePreference => ScopePreference.Current;
+
+            public OptionContainer(TOption option)
+            {
+                Option = option;
+            }
+
+            object IDirectTarget.GetValue() => Option;
+
+            bool ITarget.SupportsType(Type type) => typeof(OptionContainer<TService, TOption>) == type;
+        }
+
+        public static ITargetContainer SetOption<TOption>(this ITargetContainer targets, TOption option)
+            where TOption : class
+        {
+            if (targets == null) throw new ArgumentNullException(nameof(targets));
+            if (option == null) throw new ArgumentNullException(nameof(option));
+            targets.Register(new OptionContainer<TOption>(option));
+            return targets;
+        }
+
+        public static ITargetContainer SetOption<TOption>(this ITargetContainer targets, Type serviceType, TOption option)
+            where TOption : class
+        {
+            if (targets == null) throw new ArgumentNullException(nameof(targets));
+            if (serviceType == null) throw new ArgumentNullException(nameof(serviceType));
+            if (option == null) throw new ArgumentNullException(nameof(option));
+
+            ITarget optContainer = (ITarget)Activator.CreateInstance(typeof(OptionContainer<,>).MakeGenericType(serviceType, typeof(TOption)), option);
+            targets.Register(optContainer);
+
+            return targets;
+        }
+
+        public static ITargetContainer SetOption<TService, TOption>(this ITargetContainer targets, TOption option)
+            where TOption : class
+        {
+            if (targets == null) throw new ArgumentNullException(nameof(targets));
+            if (option == null) throw new ArgumentNullException(nameof(option));
+            targets.Register(new OptionContainer<TService, TOption>(option));
+
+            return targets;
+        }
+
+        public static TOption GetOption<TOption>(this ITargetContainer targets, TOption @default = default(TOption))
+            where TOption : class
+        {
+            if (targets == null) throw new ArgumentNullException(nameof(targets));
+            var optionContainer = targets.FetchDirect<OptionContainer<TOption>>();
+            return optionContainer?.Option ?? @default;
+        }
+
+        public static TOption GetOption<TOption>(this ITargetContainer targets, Type serviceType, TOption @default = default(TOption))
+            where TOption : class
+        {
+            if (targets == null) throw new ArgumentNullException(nameof(targets));
+            if (serviceType == null) throw new ArgumentNullException(nameof(serviceType));
+
+            return default(TOption);
+        }
+
+        public static TOption GetOption<TService, TOption>(this ITargetContainer targets, TOption @default = default(TOption))
+            where TOption : class
+        {
+            if (targets == null) throw new ArgumentNullException(nameof(targets));
+            return GetOption<TOption>(targets, typeof(TService), @default);
+        }
+    }
+
+    public class Option<T>
+    {
+        public T Value { get; protected set; }
+
+        public static implicit operator T(Option<T> option)
+        {
+            return option != null ? option.Value : default(T);
+        }
+    }
+
+    public class AllowMultiple : Option<bool>
+    {
+        public static implicit operator AllowMultiple(bool value)
+        {
+            return new AllowMultiple() { Value = value };
+        }
+    }
+}
---
+++ test/Rezolver.Tests/TargetContainerConfigurationTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Rezolver.Tests
+{
+    public class TargetContainerConfigurationTests
+    {
+        [Fact]
+        public void Configuration()
+        {
+            var targets = new TargetContainer();
+
+            targets.SetOption<AllowMultiple>(false)
+                .SetOption<int, AllowMultiple>(true);
+
+            //bool allowMultiple = targets.
+        }
+
+
+    }
+}
