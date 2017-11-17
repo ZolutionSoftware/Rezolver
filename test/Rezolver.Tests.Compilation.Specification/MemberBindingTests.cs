b204552e81e433be43a287474b1b73108c8dbc99
Andras Zoltan <andras@zoltanclan.co.uk>
2017-11-17 14:11:31 +0000

    getting closer to a final implementation of the memberbinding registration fluent API

--- test/Rezolver.Tests.Compilation.Specification/MemberBindingTests.cs
+++ test/Rezolver.Tests.Compilation.Specification/MemberBindingTests.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -10,141 +11,123 @@ using static Rezolver.TypeExtensions;
 
 namespace Rezolver.Tests.Compilation.Specification
 {
+    public interface IBuilder<T>
+    {
+        T Build();
+    }
+
+    public interface IMemberBindingsBuilder<TInstance> : IBuilder<IMemberBindingBehaviour>
+    {
+        MemberBindingBuilder<TInstance, TMember> Bind<TMember>(Expression<Func<TInstance, TMember>> memberBindingExpression);
+    }
+
+    public class MemberBindingBuilder<TInstance, TMember> : IBuilder<MemberBinding>, IBuilder<IMemberBindingBehaviour>
+    {
+        private static readonly BindableCollectionType BindableCollectionType = typeof(TMember).GetBindableCollectionTypeInfo();
+
+        private readonly System.Reflection.MemberInfo _member;
+        private readonly MemberBindingsBuilder<TInstance> _parent;
+        private Func<MemberBinding> _bindingFactory;
+
+        internal MemberBindingBuilder(MemberInfo member, MemberBindingsBuilder<TInstance> parent)
+        {
+            _member = member;
+            _parent = parent;
+        }
+
+        public IMemberBindingsBuilder<TInstance> SetFactory(Func<MemberBinding> factory)
+        {
+            if (_bindingFactory != null)
+                throw new InvalidOperationException("Binding factory has already been set");
+
+            _bindingFactory = factory;
+            return _parent;
+        }
+
+        public IMemberBindingsBuilder<TInstance> ToType<TTarget>()
+            where TTarget : TMember
+        {
+            return SetFactory(() => new MemberBinding(_member, typeof(TTarget)));
+        }
+
+        public IMemberBindingsBuilder<TInstance> ToType(Type type)
+        {
+            return SetFactory(() => new MemberBinding(_member, type));
+        }
+
+        public IMemberBindingsBuilder<TInstance> ToTarget(ITarget target)
+        {
+            return SetFactory(() => new MemberBinding(_member, target));
+        }
+
+        public IMemberBindingsBuilder<TInstance> AsCollection()
+        {
+            ValidateCollectionType();
+            return AsCollectionInternal((Type)null);
+        }
+
+        public IMemberBindingsBuilder<TInstance> AsCollection<TElement>()
+        {
+            ValidateCollectionType();
+            return AsCollection(typeof(TElement));
+        }
+
+        public IMemberBindingsBuilder<TInstance> AsCollection(Type elementType)
+        {
+            ValidateCollectionType();
+            return AsCollectionInternal(elementType);
+        }
+
+        private void ValidateCollectionType()
+        {
+            if (BindableCollectionType == null)
+                throw new InvalidOperationException($"The type { typeof(TMember) } is not a type suitable for collection initialisation.");
+        }
+
+        private IMemberBindingsBuilder<TInstance> AsCollectionInternal(Type elementType)
+            => AsCollectionInternal(Target.Resolved(typeof(IEnumerable<>).MakeGenericType(elementType ?? BindableCollectionType.ElementType)));
+
+        private IMemberBindingsBuilder<TInstance> AsCollectionInternal(ITarget target)
+            => SetFactory(() => new ListMemberBinding(_member, target, BindableCollectionType.ElementType, BindableCollectionType.AddMethod));
+
+        public MemberBindingBuilder<TInstance, TNextMember> Bind<TNextMember>(Expression<Func<TInstance, TNextMember>> memberBindingExpression)
+            => _parent.Bind(memberBindingExpression ?? throw new ArgumentNullException(nameof(memberBindingExpression)));
+
+        IMemberBindingBehaviour IBuilder<IMemberBindingBehaviour>.Build() => _parent.Build();
+
+        MemberBinding IBuilder<MemberBinding>.Build() => _bindingFactory?.Invoke() ?? new MemberBinding(_member);
+    }
+
     public class MemberBindingsBuilder<TInstance> : IMemberBindingsBuilder<TInstance>
     {
-        private ITargetContainer _targets;
-        private List<MemberBinding> _bindings;
-        private HashSet<Guid> _boundFactories;
+        private Dictionary<MemberInfo, IBuilder<MemberBinding>> _bindingBuilders;
 
-        public MemberBindingsBuilder(ITargetContainer targets)
+        public MemberBindingsBuilder()
         {
-            _targets = targets;
-            _bindings = new List<MemberBinding>();
-            _boundFactories = new HashSet<Guid>();
+            _bindingBuilders = new Dictionary<MemberInfo, IBuilder<MemberBinding>>();
         }
 
-        public IEnumerable<MemberBinding> Bindings { get => _bindings.AsReadOnly(); }
-
-        public IMemberBindingBehaviour CreateBehaviour()
-        {
-            return new BindSpecificMembersBehaviour(_bindings);
-        }
-
-        public class MemberBindingBuilder<TMember> : IMemberBindingsBuilder<TInstance>
-        {
-            private static readonly BindableCollectionType BindableCollectionType = typeof(TMember).GetBindableCollectionTypeInfo();
-
-            private readonly Guid _id = Guid.NewGuid();
-            private readonly System.Reflection.MemberInfo _member;
-            private readonly MemberBindingsBuilder<TInstance> _parent;
-
-            internal MemberBindingBuilder(System.Reflection.MemberInfo member, MemberBindingsBuilder<TInstance> parent)
-            {
-                _member = member;
-                _parent = parent;
-            }
-
-            private MemberBindingsBuilder<TInstance> AddToParent(MemberBinding binding)
-            {
-                // TODO: Change this so the binding is always written - either to a new slot,
-                // or overwriting the last-registered binding for the same member.  Then we
-                // can insta-add bindings as soon as Bind() is called, removing the need for the Auto() 
-                // method.
-                if (_parent._boundFactories.Contains(_id))
-                    throw new InvalidOperationException("Cannot bind the same member twice");
-
-                _parent._bindings.Add(binding);
-                _parent._boundFactories.Add(_id);
-                return _parent;
-            }
-
-            public static implicit operator MemberBinding(MemberBindingBuilder<TMember> factory)
-            {
-                return new MemberBinding(factory._member);
-            }
-
-            public MemberBindingsBuilder<TInstance> Auto()
-            {
-                return AddToParent(this);
-            }
-
-            public MemberBindingsBuilder<TInstance> ToType<TTarget>()
-                where TTarget : TInstance
-            {
-                return AddToParent(new MemberBinding(_member, typeof(TTarget)));
-            }
-
-            public MemberBindingsBuilder<TInstance> ToType(Type type)
-            {
-                return AddToParent(new MemberBinding(_member, type));
-            }
-
-            public MemberBindingsBuilder<TInstance> ToTarget(ITarget target)
-            {
-                return AddToParent(new MemberBinding(_member, target));
-            }
-
-            public MemberBindingsBuilder<TInstance> AsCollection()
-            {
-                ValidateCollectionType();
-                return AsCollectionInternal((Type)null);
-            }
-
-            public MemberBindingsBuilder<TInstance> AsCollection<TElement>()
-            {
-                ValidateCollectionType();
-                return AsCollection(typeof(TElement));
-            }
-
-            public MemberBindingsBuilder<TInstance> AsCollection(Type elementType)
-            {
-                ValidateCollectionType();
-                return AsCollectionInternal(elementType);
-            }
-
-            private void ValidateCollectionType()
-            {
-                if (BindableCollectionType == null)
-                    throw new InvalidOperationException($"The type { typeof(TMember) } is not a type suitable for collection initialisation.");
-            }
-
-            private MemberBindingsBuilder<TInstance> AsCollectionInternal(Type elementType)
-            {
-                return AsCollectionInternal(Target.Resolved(typeof(IEnumerable<>).MakeGenericType(elementType ?? BindableCollectionType.ElementType)));
-            }
-
-            private MemberBindingsBuilder<TInstance> AsCollectionInternal(ITarget target)
-            {
-                return AddToParent(new ListMemberBinding(_member, target, BindableCollectionType.ElementType, BindableCollectionType.AddMethod));
-            }
-
-            public MemberBindingBuilder<TMember1> Bind<TMember1>(Expression<Func<TInstance, TMember1>> memberBindingExpression)
-            {
-                return AddToParent(this).Bind(memberBindingExpression);
-            }
-
-            IMemberBindingBehaviour IMemberBindingsBuilder<TInstance>.CreateBehaviour()
-            {
-                return AddToParent(this).CreateBehaviour();
-            }
-        }
-
-        public MemberBindingBuilder<TMember> Bind<TMember>(Expression<Func<TInstance, TMember>> memberBindingExpression)
+        public MemberBindingBuilder<TInstance, TMember> Bind<TMember>(Expression<Func<TInstance, TMember>> memberBindingExpression)
         {
             var member = MethodCallExtractor.ExtractMemberAccess(memberBindingExpression);
             if (member == null)
                 throw new ArgumentException($"The expression {{{ memberBindingExpression }}} must have a member access expression as its body, and it must be a member belonging to the type { typeof(TInstance) }", nameof(memberBindingExpression));
-
-            return new MemberBindingBuilder<TMember>(member, this);
+            if (_bindingBuilders.ContainsKey(member))
+                throw new ArgumentException($"The member represented by the expression {{{ memberBindingExpression }}} has already been bound", nameof(memberBindingExpression));
+            var toReturn = new MemberBindingBuilder<TInstance, TMember>(member, this);
+            _bindingBuilders[member] = toReturn;
+            return toReturn; ;
         }
+
+        public IMemberBindingBehaviour Build() => new BindSpecificMembersBehaviour(_bindingBuilders.Values.Select(b => b.Build()));
     }
     public static class ExtraRegistrationExtensions
     {
         public static void RegisterType<TInstance>(this ITargetContainer targets, Action<MemberBindingsBuilder<TInstance>> bindingFactory)
         {
-            var factory = new MemberBindingsBuilder<TInstance>(targets);
+            var factory = new MemberBindingsBuilder<TInstance>();
             bindingFactory?.Invoke(factory);
-            targets.RegisterType<TInstance>(factory.CreateBehaviour());
+            targets.RegisterType<TInstance>(factory.Build());
         }
     }
 
@@ -174,7 +157,10 @@ namespace Rezolver.Tests.Compilation.Specification
             // Arrange
             var targets = CreateTargetContainer();
             targets.RegisterObject(10);
-            targets.RegisterType<HasCollectionMember>(f => f.Bind(hcm => hcm.Numbers));
+            targets.RegisterType<HasCollectionMember>(f => 
+                f.Bind(hcm => hcm.Numbers)
+                .Bind(hcm => hcm.Numbers).AsCollection()
+                .Bind(hcm => hcm.Numbers).ToType<System.Collections.ObjectModel.ReadOnlyCollection<int>>());
 
             var container = CreateContainer(targets);
 
