8d09d74c34240c8a990e789727be0864ed1bd0f4
Andras Zoltan <andras@zoltanclan.co.uk>
2018-11-20 00:20:33 +0000

    should be able to delete the old docs website now.  Seems to work a lot of better as part of a .net core build.

--- doc/Rezolver.Docs/gulpfile.js
+++ doc/Rezolver.Docs/gulpfile.js
@@ -17,128 +17,128 @@ var sitemap = require('gulp-sitemap');
 
 //copied only
 var bootstrap = {
-  src: ['**/*'], cwd: 'bower_components/bootstrap/less/',
-  font: {
-    src: ['*'],
-    cwd: 'bower_components/bootstrap/dist/fonts/'
-  }
+    src: ['**/*'], cwd: 'bower_components/bootstrap/less/',
+    font: {
+        src: ['*'],
+        cwd: 'bower_components/bootstrap/dist/fonts/'
+    }
 };
 
 var fontawesome = {
-  src: ['**/*'], cwd: 'bower_components/components-font-awesome/less/',
-  font: {
-    src: ['*'],
-    cwd: 'bower_components/components-font-awesome/fonts/'
-  }
+    src: ['**/*'], cwd: 'bower_components/components-font-awesome/less/',
+    font: {
+        src: ['*'],
+        cwd: 'bower_components/components-font-awesome/fonts/'
+    }
 };
 //built
 var rezolverBootstrap = {
-  src: 'site.less',
-  cwd: 'styles/'
+    src: 'site.less',
+    cwd: 'wwwroot/styles/'
 };
 var rezolverDocFXMain = {
-  src: 'main.less',
-  dest: ['main.*'],
-  cwd: '_docfx_themes/rezolver/styles/'
+    src: 'main.less',
+    dest: ['main.*'],
+    cwd: '_docfx_themes/rezolver/styles/'
 };
 
 //bundling and minification of the docfx custom template theme - the
 //files which replace the default docfx.vendor.* files.
 var vendor = {
-    css: ['styles/site.css' //built from site.less in less task after copy
-                            //includes: custom bootstrap, 
-                            //custom highlightjs theme and font-awesome
+    css: ['wwwroot/styles/site.css' //built from site.less in less task after copy
+        //includes: custom bootstrap, 
+        //custom highlightjs theme and font-awesome
     ],
     js: ['bower_components/jquery/dist/jquery.min.js',
-         'bower_components/bootstrap/dist/js/bootstrap.min.js',
-         'bower_components/highlightjs/highlight.pack.min.js',
-         'bower_components/lunr.js/lunr.min.js',
-         'bower_components/js-url/url.min.js',
-         'bower_components/twbs-pagination/jquery.twbsPagination.min.js',
-         "bower_components/mark.js/dist/jquery.mark.min.js",
-         "bower_components/anchor-js/anchor.min.js"
+        'bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'bower_components/highlightjs/highlight.pack.min.js',
+        'bower_components/lunr.js/lunr.min.js',
+        'bower_components/js-url/url.min.js',
+        'bower_components/twbs-pagination/jquery.twbsPagination.min.js',
+        "bower_components/mark.js/dist/jquery.mark.min.js",
+        "bower_components/anchor-js/anchor.min.js"
     ],
     dest: ['docfx.vendor.*']
 };
 
 var devSitemap = {
-    src: ['./developers/**/*.html', '!./developers/**/toc.html'],
-    dest: './developers',
-    siteUrl: 'http://rezolver.co.uk/developers'
+    src: ['./wwwroot/developers/**/*.html', '!./wwwroot/developers/**/toc.html'],
+    dest: './wwwroot/developers',
+    siteUrl: 'http://pre.rezolver.co.uk/developers'
 };
 
 gulp.task('sitemap', function () {
     return gulp.src(devSitemap.src, {
         read: false
     })
-    .pipe(sitemap({
-        siteUrl: devSitemap.siteUrl
-    }))
-    .pipe(gulp.dest(devSitemap.dest));
+        .pipe(sitemap({
+            siteUrl: devSitemap.siteUrl
+        }))
+        .pipe(gulp.dest(devSitemap.dest));
 });
 
 //destroys all the output and intermediate files produced by the docfx processing - this is to ensure that
 //APIs and pages which are no longer being produced are no longer present in the content.
 gulp.task('cleanDocFXOutput', function () {
-  return del(['./developers/**', './obj/**', './_docfx_proj/obj/**', '!./developers']);
+    return del(['./wwwroot/developers/**', '!./developers']);
 });
 
 gulp.task('copy', function () {
-  return merge(
-    gulp.src(bootstrap.src, { cwd: bootstrap.cwd })
-      .pipe(gulp.dest('./styles/bootstrap/')),
-    gulp.src(fontawesome.src, { cwd: fontawesome.cwd })
-      .pipe(gulp.dest('./styles/fontawesome/')),
-      //fonts are copied to two places to satisfy both the main site's
-      //styles and the Rezolver docfx theme's styles.
-    gulp.src(bootstrap.font.src, { cwd: bootstrap.font.cwd })
-      .pipe(gulp.dest('./styles/fonts'))
-      .pipe(gulp.dest('./_docfx_themes/rezolver/styles/fonts/')),
-      gulp.src(fontawesome.font.src, { cwd: fontawesome.font.cwd })
-      //fonts are copied to two places to satisfy both the main site's
-      //styles and the Rezolver docfx theme's styles.
-      .pipe(gulp.dest('./styles/fonts'))
-      .pipe(gulp.dest('./_docfx_themes/rezolver/styles/fonts/'))
-  );
+    return merge(
+        gulp.src(bootstrap.src, { cwd: bootstrap.cwd })
+            .pipe(gulp.dest('./wwwroot/styles/bootstrap/')),
+        gulp.src(fontawesome.src, { cwd: fontawesome.cwd })
+            .pipe(gulp.dest('./wwwroot/styles/fontawesome/')),
+        //fonts are copied to two places to satisfy both the main site's
+        //styles and the Rezolver docfx theme's styles.
+        gulp.src(bootstrap.font.src, { cwd: bootstrap.font.cwd })
+            .pipe(gulp.dest('./wwwroot/styles/fonts'))
+            .pipe(gulp.dest('./_docfx_themes/rezolver/styles/fonts/')),
+        gulp.src(fontawesome.font.src, { cwd: fontawesome.font.cwd })
+            //fonts are copied to two places to satisfy both the main site's
+            //styles and the Rezolver docfx theme's styles.
+            .pipe(gulp.dest('./wwwroot/styles/fonts'))
+            .pipe(gulp.dest('./_docfx_themes/rezolver/styles/fonts/'))
+    );
 });
 
 //single 'less' task compiles our less files with source maps
 gulp.task('less', ['copy'], function () {
-  return merge(
-    //builds the rezolver bootstrap theme from the less in ./styles/
-    gulp.src(rezolverBootstrap.src, { cwd: rezolverBootstrap.cwd })
-      .pipe(sourcemaps.init())
-      .pipe(less({
-        plugins: [autoprefix]
-      }))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(rezolverBootstrap.cwd)),
-    gulp.src(rezolverDocFXMain.src, { cwd: rezolverDocFXMain.cwd })
-      .pipe(sourcemaps.init())
-      .pipe(less({
-        plugins: [autoprefix]
-      }))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(rezolverDocFXMain.cwd))
+    return merge(
+        //builds the rezolver bootstrap theme from the less in ./styles/
+        gulp.src(rezolverBootstrap.src, { cwd: rezolverBootstrap.cwd })
+            .pipe(sourcemaps.init())
+            .pipe(less({
+                plugins: [autoprefix]
+            }))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(rezolverBootstrap.cwd)),
+        gulp.src(rezolverDocFXMain.src, { cwd: rezolverDocFXMain.cwd })
+            .pipe(sourcemaps.init())
+            .pipe(less({
+                plugins: [autoprefix]
+            }))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(rezolverDocFXMain.cwd))
     );
 });
 
 gulp.task('bundles', ['less'], function () {
-  return merge(
-    gulp.src(vendor.css)
-      .pipe(sourcemaps.init())
-      .pipe(minify({ keepBreaks: true }))
-      .pipe(rename({
-        suffix: '.min'
-      }))
-      .pipe(concat('docfx.vendor.css'))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(rezolverDocFXMain.cwd)),
-    gulp.src(vendor.js)
-      .pipe(sourcemaps.init())
-      .pipe(concat('docfx.vendor.js'))
-      .pipe(sourcemaps.write('.'))
-      .pipe(gulp.dest(rezolverDocFXMain.cwd))
+    return merge(
+        gulp.src(vendor.css)
+            .pipe(sourcemaps.init())
+            .pipe(minify({ keepBreaks: true }))
+            .pipe(rename({
+                suffix: '.min'
+            }))
+            .pipe(concat('docfx.vendor.css'))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(rezolverDocFXMain.cwd)),
+        gulp.src(vendor.js)
+            .pipe(sourcemaps.init())
+            .pipe(concat('docfx.vendor.js'))
+            .pipe(sourcemaps.write('.'))
+            .pipe(gulp.dest(rezolverDocFXMain.cwd))
     );
 });
 
@@ -150,6 +150,6 @@ gulp.task('default', ['bundles']);
 //task and then copies everything to the /developers/styles folder - which is usually
 //performed by the docfx build courtesy of its template handling.
 gulp.task('patchtemplate', ['default'], function () {
-  return gulp.src([].concat(rezolverDocFXMain.dest).concat(vendor.dest), { cwd: rezolverDocFXMain.cwd })
-      .pipe(gulp.dest('./developers/styles'));
+    return gulp.src([].concat(rezolverDocFXMain.dest).concat(vendor.dest), { cwd: rezolverDocFXMain.cwd })
+        .pipe(gulp.dest('./wwwroot/developers/styles'));
 });
