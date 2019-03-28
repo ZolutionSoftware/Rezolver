af916f8a1f375784fde0fe3509ee78616c69f04a
Andras Zoltan <andras@zoltanclan.co.uk>
2019-03-28 09:17:33 +0000

    rubbish!

--- src/Rezolver/Compilation/Expressions/SingletonTargetBuilder.cs
+++ src/Rezolver/Compilation/Expressions/SingletonTargetBuilder.cs
@@ -34,7 +34,7 @@ namespace Rezolver.Compilation.Expressions
             TypeAndTargetId id = new TypeAndTargetId(context.TargetType ?? target.DeclaredType, targetIdOverride ?? target.Id);
 
             var lazy = holder.GetLazy(id);
-
+            // test
             if(lazy == null)
             {
                 holder.GetLazy(
